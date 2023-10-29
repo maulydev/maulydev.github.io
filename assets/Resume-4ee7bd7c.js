5524b5d8254aa116e3957b4e8d9f4427045849ef
Ernest Kumashie <mauly.dev@gmail.com>
2023-10-28 23:01:11 -0700

    Updates

---
+++ assets/Resume-4ee7bd7c.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-2a2524a3.js";const e="/assets/resume-681f75c2.pdf",t=()=>s.jsx("embed",{src:e,className:"w-full h-screen"});export{t as default};
--- assets/Resume-8625d7f1.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-b296aa8a.js";const e="/assets/resume-681f75c2.pdf",t=()=>s.jsx("embed",{src:e,className:"w-full h-screen"});export{t as default};
