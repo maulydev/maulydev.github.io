ff71e877454b5b695061633ffa42d8f15cd44e8a
Ernest Kumashie <mauly.dev@gmail.com>
2023-11-11 13:49:13 -1200

    Update vite.config.js
    For legacy browser support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
-  plugins: [react()],
-})
+  esbuild: {
+    target: "es2015",
+    include: /\.(ts|jsx|tsx)$/,
+  },
+  build: {
+    target: "es2015",
+    cssTarget: "chrome61",
+  },
+  plugins: [
+    react(),
+    legacy({
+      targets: [
+        "chrome >= 64",
+        "edge >= 79",
+        "safari >= 11.1",
+        "firefox >= 67",
+      ],
+      renderLegacyChunks: false,
+      modernPolyfills: true,
+    }),
+  ],
+});
