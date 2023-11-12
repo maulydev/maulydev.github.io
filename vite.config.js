cec275df70083af2e08466d1a937bac33990ae0e
Ernest Kumashie <mauly.dev@gmail.com>
2023-11-11 14:26:21 -1200

    Updated and deployed

--- vite.config.js
+++ vite.config.js
@@ -3,30 +3,36 @@ import react from "@vitejs/plugin-react";
 import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 export default defineConfig({
-  esbuild: {
-    target: "es2015",
-    include: /\.(ts|jsx|tsx)$/,
-  },
-  build: {
-    target: "es2015",
-    cssTarget: "chrome61",
-  },
   plugins: [
     react(),
+
     legacy({
-      targets: [
-        "chrome >= 64",
-        "edge >= 79",
-        "safari >= 11.1",
-        "firefox >= 67",
-      ],
-      renderLegacyChunks: false,
-      modernPolyfills: true,
+      targets: ["defaults", "not IE 11"],
     }),
   ],
 });
+
+// export default defineConfig({
+//   esbuild: {
+//     target: "es2015",
+//     include: /\.(ts|jsx|tsx)$/,
+//   },
+//   build: {
+//     target: "es2015",
+//     cssTarget: "chrome61",
+//   },
+//   plugins: [
+//     react(),
+//     legacy({
+//       targets: [
+//         "chrome >= 64",
+//         "edge >= 79",
+//         "safari >= 11.1",
+//         "firefox >= 67",
+//       ],
+//       renderLegacyChunks: false,
+//       modernPolyfills: true,
+//     }),
+//   ],
+// });
